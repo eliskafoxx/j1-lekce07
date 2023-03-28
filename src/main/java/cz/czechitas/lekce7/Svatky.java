8d8fac8796b1a8becab3ca21a86953a494df537b
eliskafoxx <eliska.foxx@gmail.com>
2023-03-28 19:06:19 +0200

    toDo Svatky hotovo

--- src/main/java/cz/czechitas/lekce7/Svatky.java
+++ src/main/java/cz/czechitas/lekce7/Svatky.java
@@ -59,8 +59,7 @@ public class Svatky {
    * @return Den a měsíc, případně {@code null}, pokud jméno nebylo nalezeno.
    */
   public MonthDay kdyMaSvatek(String jmeno) {
-    //TODO
-    return null;
+    return svatky.get(jmeno);
   }
 
   /**
@@ -69,8 +68,7 @@ public class Svatky {
    * @return {@code true}, pokud je jméno v seznamu. Jinak vrací {@code false}.
    */
   public boolean jeVSeznamu(String jmeno) {
-    //TODO
-    return false;
+    return svatky.containsKey(jmeno);
   }
 
   /**
@@ -78,8 +76,7 @@ public class Svatky {
    * @return
    */
   public int getPocetJmen() {
-    //TODO
-    return 0;
+    return svatky.size();
   }
 
   /**
@@ -87,8 +84,7 @@ public class Svatky {
    * @return Neseřazený seznam jmen.
    */
   public Set<String> getSeznamJmen() {
-    //TODO
-    return null;
+    return svatky.keySet();
   }
 
   /**
@@ -97,7 +93,7 @@ public class Svatky {
    * @param denMesic Den a měsíc, kdy má dané jméno svátek.
    */
   public void pridatSvatek(String jmeno, MonthDay denMesic) {
-    //TODO
+    svatky.put(jmeno, denMesic);
   }
 
   /**
@@ -107,7 +103,7 @@ public class Svatky {
    * @param mesic Měsíc, kdy má dané jméno svátek (1–12).
    */
   public void pridatSvatek(String jmeno, int den, int mesic) {
-    //TODO
+    svatky.put(jmeno,MonthDay.of(mesic, den));
   }
 
   /**
@@ -117,7 +113,7 @@ public class Svatky {
    * @param mesic Měsíc, kdy má dané jméno svátek.
    */
   public void pridatSvatek(String jmeno, int den, Month mesic) {
-    //TODO
+    svatky.put(jmeno,MonthDay.of(mesic, den));
   }
 
   /**
@@ -125,6 +121,6 @@ public class Svatky {
    * @param jmeno Jméno ke smazání.
    */
   public void smazatSvatek(String jmeno) {
-    //TODO
+    svatky.remove(jmeno);
   }
 }
